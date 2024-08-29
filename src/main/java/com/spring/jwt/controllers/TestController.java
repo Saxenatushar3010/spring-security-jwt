f3bd0161b21f97e2b74d1c7f5d13e5ae249324e8
Tushar <saxenatushar3010@gmail.com>
2024-08-29 19:50:13 +0530

    DB connection established and tested and all repository classes implemented and tested.

--- src/main/java/com/spring/jwt/controllers/TestController.java
+++ src/main/java/com/spring/jwt/controllers/TestController.java
@@ -1,14 +1,24 @@
 package com.spring.jwt.controllers;
 
+import com.spring.jwt.entity.User;
+import com.spring.jwt.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
 public class TestController {
+    @Autowired
+    UserRepository userRepository;
+
     @GetMapping("/test")
-    public String test(){
-        return "Hello";
+    public User test(@RequestParam String name){
+        Optional<User> op=userRepository.findByUsername(name);
+        return op.get();
     }
 }
---
+++ src/main/java/com/spring/jwt/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.spring.jwt.repository;
+
+import com.spring.jwt.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role,Long> {
+    Optional<Role> findByName(String name);
+}
